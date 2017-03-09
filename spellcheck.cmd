040353f021c16f73b76656a23f436fa7fb98ac36
Rémi Bèges <remi.beges@gmail.com>
2017-03-09 09:52:11 +0100

    Update spellcheck script to use a custom personnal dict

--- spellcheck.cmd
+++ spellcheck.cmd
@@ -1,20 +1,19 @@
-hunspell -t -i UTF-8 main.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 main.tex
 
-hunspell -t -i UTF-8 ./src/1/*.tex
-hunspell -t -i UTF-8 ./src/1/sections/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/1/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/1/sections/*.tex
 
-hunspell -t -i UTF-8 ./src/2/*.tex
-hunspell -t -i UTF-8 ./src/2/sections/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/2/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/2/sections/*.tex
 
-hunspell -t -i UTF-8 ./src/3/*.tex
-hunspell -t -i UTF-8 ./src/3/sections/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/3/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/3/sections/*.tex
 
-hunspell -t -i UTF-8 ./src/4/*.tex
-hunspell -t -i UTF-8 ./src/4/sections/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/4/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/4/sections/*.tex
 
-hunspell -t -i UTF-8 ./src/5/*.tex
-hunspell -t -i UTF-8 ./src/5/sections/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/5/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/5/sections/*.tex
 
-hunspell -t -i UTF-8 ./src/appendices/*.tex
-hunspell -t -i UTF-8 ./src/conclusion/*.tex
-hunspell -t -i UTF-8 ./src/glos.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/conclusion/*.tex
+hunspell -p ./personnal_dict_spellcheck -t -i UTF-8 ./src/glos.tex
