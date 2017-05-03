4bdd841e23b8be8a073ced02647ad4bb979125cc
Rémi Bèges <remi.beges@gmail.com>
2017-05-03 09:07:27 +0200

    Fix fig generation script to avoid slowing down entire system

--- figs.js
+++ figs.js
@@ -1,19 +1,23 @@
 'use strict';
 
 var glob = require('glob');
-           require('shelljs/global');
+var shell = require('shelljs');
+var async = require('async');
 
-glob("**/*.svg", function (err, files) {
-  let amount = files.length;
-  console.log('Found ' + amount + ' SVG figures to process');
-
-  files.forEach(function(filename) {
-    let f = filename.replace('.svg','');
-    let command = 'inkscape --file='       + '"' + f + '.svg' + '"'
-                        + ' --export-pdf=' + '"' + f + '.pdf' + '"';
-
-    exec(command, function(code, stdout, stderr) {
+glob('**/*.svg', function (err, files) {
+  console.log('Found ' + files.length + ' SVG figures to process');
+    
+  async.eachOfLimit(files, 5, function(filename, key, cb) {
+	let f = filename.replace('.svg','');
+    let cmd = 'inkscape --file='       + '"' + f + '.svg' + '"'
+                    + ' --export-pdf=' + '"' + f + '.pdf' + '"';
+    shell.exec(cmd, function(code, stdout, stderr) {
+	  if (code != 0) return cb(Error(stderr));
       console.log('Fig name (' + code + '): ' + f);
+	  cb();
     });
+  }, function(err) {
+	if (err) return console.log(err);
+	console.log('All figures converted to PDF.');
   });
-})
+});
